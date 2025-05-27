4cf07850e7621c937e1aa9a03e48cb1c187b6480
Chani Kaufman <ckaufman@by.local>
2025-05-27 15:20:30 +0100

    Weather forecast API complete

--- src/WeatherTemp.js
+++ src/WeatherTemp.js
@@ -1,43 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Weather.css";
 
 export default function WeatherTemp(props) {
-  const [unit, setUnit] = useState("celsius");
-  function showCelsius(event) {
-    event.preventDefault();
-    setUnit("celsius");
-  }
-  function showFahrenheit(event) {
-    event.preventDefault();
-    setUnit("fahrenheit");
-  }
-  if (unit === "celsius") {
-    return (
-      <div className="WeatherTemp">
-        <img src={props.iconUrl} alt="Icon" className="weather-img" />
-        <span className="temperature">{props.celsius}</span>
-        <span className="unit">
-          °C |{" "}
-          <a href="/" onClick={showFahrenheit}>
-            °F
-          </a>
-        </span>
-      </div>
-    );
-  } else {
-    let fahrenheit = (props.celsius * 9) / 5 + 32;
-
-    return (
-      <div className="WeatherTemp">
-        <img src={props.iconUrl} alt="Icon" className="weather-img" />
-        <span className="temperature">{Math.round(fahrenheit)}</span>
-        <span className="unit">
-          <a href="/" onClick={showCelsius}>
-            °C
-          </a>{" "}
-          | °F
-        </span>
-      </div>
-    );
-  }
+  return (
+    <div className="WeatherTemp">
+      <img src={props.iconUrl} alt="Icon" className="weather-img" />
+      <span className="temperature">{props.celsius}</span>
+      <span className="unit">°C</span>
+    </div>
+  );
 }
